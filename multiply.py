b85cf7041a987812e2351591a3d0905d3650e172
Seyoung <seyoung2k@naver.com>
2021-10-13 19:58:24 -0700

    Revise mutiply.py

--- multiply.py
+++ multiply.py
@@ -1,51 +1,9 @@
 #empty list
 numList = []
-#get size of list
-while True:
-	try:
-		getElements = int(input("Enter number of elements: "))
-	except EOFError:
-		break
-#value of checking for invalid value input
-ch = True
+
 #result of multiplying the elements in list 
 result = 0
-#function of get elements in list
-def check(getElements):
-	'''
-	Check and add inputs to list.
-	This function is simply checking whether inputs are valid or not. If the inputs are valid, inputs can put in list.
-	If the inputs are not valid, then exit.
 
-	Parameters
-	----------
-	getElements: int
-		Users input
-
-	Return
-	------
-	Boolean
-		True: list is valid, False: list is invalid
-	
-	Example
-	-------
-	>>>check(1)
-	True
-	>>>check(w)
-	False
-	'''
-	#loop for getting elements in list
-	for i in range(0, getElements):
-		num = input()
-		#block for testing whether inputs are int or not
-		try:
-			num = int(num)
-			numList.append(num)
-		#block for get out function when input is not int
-		except ValueError:
-			print("This is not number")
-			return False
-	return True
 #function for multiplying all elements in list
 def multiply_list(numList):
 	'''
@@ -73,16 +31,18 @@ def multiply_list(numList):
 	result = 1
 	for i in numList:
 		result = result * i
-	return result
+	#show the result when the list have valid inputs
+	if(isinstance(result, int)):
+		return result
+	else:
+		return False
+#Driver
+list01 = [1, 2, 3, 7]
+list02 = [3, 2, 4, 89]
 
-ch = check(getElements)
+print(multiply_list(list01))
+print(multiply_list(list02))
 
-#show the result when the list have valid inputs
-if ch == True:
-	result = multiply_list(numList)
-	print(result)
-elif ch == False:
-	print("Invalid value")
 
 
 
