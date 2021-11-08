61340c7fb8e9e4165dbf1fd88aeffc0e7262f97e
Seyoung <seyoung2k@naver.com>
2021-11-07 19:50:47 -0800

    Able to make db and add variables to db

--- Flask/myapp/routes.py
+++ Flask/myapp/routes.py
@@ -1,6 +1,8 @@
-from myapp import myapp_obj
-from myapp.forms import CityForm
+from myapp import myapp_obj, db
+from myapp.forms import TopCities
 from flask import render_template, flash, redirect
+from myapp.models import City
+from sqlalchemy import asc
 
 @myapp_obj.route("/google")
 def sayGoogle():
@@ -10,13 +12,20 @@ def sayGoogle():
 def home():
     name = 'Seyoung'
     title = 'Top Cities'
-    posts = [{'city': 'Paris'},\
-            {'city': 'London'},\
-            {'city': 'Rome'},\
-            {'city': 'Tahiti'}]
-    form = CityForm()
-    if form.validate_on_submit():
-            flash(f'Add request for City Name {form.cityName.data}, visited={form.visited.data}')
-            return redirect('/')
+    
+    top_city = City.query.filter().order_by(City.city_rank.asc())
+    form = TopCities()
+    db.create_all()
 
-    return render_template('home.html', name=name, posts=posts, title=title, form=form)
+    if form.validate_on_submit():
+            flash(f'Add request for City Name {form.city_name.data}, visited={form.city_visited.data}')
+            flash(f'Add request for City Rank {form.city_rank.data}')
+            city_name = form.city_name.data
+            city_rank = form.city_rank.data
+            city_visited = form.city_visited.data
+            record = City(city_name, city_rank, city_visited)
+            db.session.add(record)
+            db.session.commit()
+            return redirect('/')
+        
+    return render_template('home.html', name=name, title=title, top_city = top_city, form=form)
