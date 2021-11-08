afbc7018a17b6cca393e408e57f03ac18de11ad6
Seyoung <seyoung2k@naver.com>
2021-11-07 19:48:33 -0800

    Add func(__init__)

--- Flask/myapp/models.py
+++ Flask/myapp/models.py
@@ -1,5 +1,15 @@
 from myapp import db
 
 class City(db.Model):
-    name = db.Column(db.String(64), unique=True, index=True)
-    rank = db.Column(db.Integer, primary_key=True)
+    city_name = db.Column(db.String(64), unique=True, index=True)
+    city_rank = db.Column(db.Integer, primary_key=True)
+    city_visited = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return '<City {}>' .format(self.city_name)
+    
+    def __init__(self, city_name, city_rank, city_visited):
+        self.city_name = city_name
+        self.city_rank = city_rank
+        self.city_visited = city_visited
+
