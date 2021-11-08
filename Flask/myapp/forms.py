86c84afb0b30b4bd34d3d783116435219752fa73
Seyoung <seyoung2k@naver.com>
2021-11-07 19:49:42 -0800

    Modify city_rank- (add validators)

--- Flask/myapp/forms.py
+++ Flask/myapp/forms.py
@@ -1,10 +1,9 @@
-#forms.py
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
-class CityForm(FlaskForm):
-    cityName = StringField('City Name', validators=[DataRequired()])
-    cityRank = IntegerField('City Rank')
-    visited = BooleanField('Visited')
+class TopCities(FlaskForm):
+    city_name = StringField('City Name', validators=[DataRequired()])
+    city_rank = IntegerField('City Rank', validators=[DataRequired()])
+    city_visited = BooleanField('Visited', default=False)
     submit = SubmitField('Submit')
